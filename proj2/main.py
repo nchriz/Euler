19e06a61cd1a824272632208b77fb16ca7aea45d
nchriz <chma00@gmail.com>
2014-09-23 13:16:00 +0200

    proj2 python

---
+++ proj2/main.py
@@ -0,0 +1,23 @@
+import math
+from math import *
+
+def main():
+    n = 4*pow(10,6)
+    sum = 0
+    i = 1
+
+    fib = []
+    fib.append(1)
+    fib.append(1)
+
+    while fib[i] < n:
+        i+=1
+        fib.append(fib[i-1] + fib[i-2])
+        sum += fib[i] if (fib[i]%2)==0 else 0
+
+    #for x in xrange(0,i):
+        #sum += fib[x] if (fib[x]%2)==0 else 0
+    print(sum)
+
+if __name__ == "__main__":
+    main()
