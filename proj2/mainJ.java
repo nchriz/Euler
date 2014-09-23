4f63220b008ba4c5ff7db8e17f8403097146546a
nchriz <chma00@gmail.com>
2014-09-23 16:36:00 +0200

    Change Java without tmp file

--- proj2/mainJ.java
+++ proj2/mainJ.java
@@ -2,18 +2,14 @@ public class mainJ {
     public static void main(String[] args){
 	double n = 4.0 * Math.pow(10,6);
 	int x = 1, y = 1;
-	int tmp, sum = 0;
+	int sum = 0;
 
 	while(y < n){
-	    tmp = x;
-	    x = y;
-	    y = y + tmp;
+	    y = y + x;
+	    x = y - x;
 
 	    sum += ((y%2)==0) ? y : 0;
 	}
-
 	System.out.println(sum);
-
     }
-
 }
