3c87785723b7f64353898329e3e5466a4aaaf25b
nchriz <chma00@gmail.com>
2014-09-28 18:30:03 +0200

    Java working, fast 19 ms

--- proj7/mainJ.java
+++ proj7/mainJ.java
@@ -1,40 +1,37 @@
 public class mainJ{
 
-    public static int isPrime(int n){
+    public static boolean[] isPrime(int n){
 	int upper = (int) Math.sqrt(n)+1;
 	boolean[] primes = new boolean[n];
 
+	for(int i = 2; i <n; i++)
+	    primes[i] = true;
 
-	for(int can = 3; can < upper; can++)
-	    if(!primes[can])
+
+	for(int can = 2; can < upper; can++)
+	    if(primes[can])
 		for(int j = 2*can; j < n; j+=can)
-		    primes[j]=true;
+		    primes[j]=false;
 
-	for(int i = 0; i < n; i++)
-	    System.out.println(primes[i] + " + " + i);
-
-	return 1;
-    }
-
-    public static int prime(int n){
-	int upper = (int) Math.sqrt(n);
-	boolean[] sieve = new boolean[n+1];
-	for(int i = 2; i <= upper; i++)
-	    if(!sieve[i])
-	        for(int k = i*i; k <= upper; k+=i)
-		    sieve[k] = true;
-	int[] intsieve = new int[n];
-	for(int i = 3; i <= upper; i++)
-	    if(!sieve[i])
-		intsieve[i] = i;
-	System.out.println(sieve[10]);
-	return 1;//sieve[10001];
-
+	//for(int i = 0; i < n; i++)
+	//    System.out.println(primes[i] + " + " + i);
+	
+	return primes;
     }
 
     public static void main(String[] args){
         int N = Integer.parseInt(args[0]);
-	//System.out.println(prime(2*1000000));
-	isPrime(N);
+	int[] num = new int[N];
+	int tmp = 0;
+	long start = System.currentTimeMillis();
+	boolean[] primes = isPrime(N);
+	for(int i = 0; i < N; i++)
+	    if(primes[i]){
+		num[tmp] = i;
+		tmp++;
+	    }
+	System.out.println(num[10001]);
+	System.out.println(System.currentTimeMillis()-start);
+		
     }
 }
