52999fc3190bbd4abf47391da0c981e7b10c0cbc
chris <chma00@gmail.com>
2014-10-05 17:30:21 +0200

    working, almost fast

--- proj21/test.py
+++ proj21/test.py
@@ -1,11 +1,21 @@
+import time
+from time import *
+
 def main():
-    n = 10**5
+    start = time()
+    n = 10**6
+    sum = 0
     num = [1] *n
     for x in range(2,int(n/2)):
         for y in range(2*x,n,x):
             num[y] += x
 
-    print num
+    for x in range(2,10001):
+        if x == num[num[x]] and num[x] != x:
+            sum += x
+
+    print time() - start
+    print sum
         
             
 
