dccaace925ca9ec1dbbaf2c87d94f5676012c25d
chris <chma00@gmail.com>
2014-10-05 15:54:56 +0200

    Java working better

--- proj20/mainJ.java
+++ proj20/mainJ.java
@@ -7,14 +7,11 @@ public class mainJ {
 	BigInteger sum = BigInteger.ONE;
 	for(int i = 2; i < n; i++)
 	    sum = sum.multiply(BigInteger.valueOf(i));
-	
-	System.out.println(sum.toString());
-
+	String s = sum.toString(10);
 	int tmp = 0;
-	while(sum.intValue() > 1){
-	    tmp += sum.intValue()%10;
-	    sum = sum.divide(BigInteger.valueOf(10));
-	}
+
+	for(int i=0; i < s.length();i++)
+	    tmp += Integer.parseInt(s.substring(i,i+1));
 
 	System.out.println(tmp);
 
